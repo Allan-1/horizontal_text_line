b85ee75d3e673bc83c946489b94d7fdb9427d209
Allan Muturi <amuturi01@gmail.com>
2023-02-16 12:45:25 +0300

    initalize test

--- test/horizontal_text_line_test.dart
+++ test/horizontal_text_line_test.dart
@@ -1,7 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:horizontal_text_line/horizontal_text_line.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-
+  testWidgets("Widget has two dividers and a text", (widgetTester) async {
+    await widgetTester.pumpWidget(HorizontalTextLine(
+      text: "Or",
+      color: Colors.brown,
+      height: 2,
+    ));
+  });
 }
